7d4df15d81ab2d23c256fb65ca00c0962a3dc7fc
Anastasiia Shrai <nastia.shrai@gmail.com>
2021-03-13 10:50:04 +0200

    fix: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = bracketsConfig.map(el => el.join(''));
+
+  for (let i = 0; i < brackets.length; ) {
+    if (str.includes(brackets[i])) {
+      str = str.replace(brackets[i], '');
+      i = 0;
+    }
+    else i++
+  
+  }
+  return str === '';
 }
